7ac3cd67679e44295f9775478c100114c29b80f6
JuanDiSIlva <silvachaves@unicauca.edu.co>
2023-11-29 19:59:37 -0500

    Aparecer y desaparacer menu mobile

--- mains.js
+++ mains.js
@@ -1,15 +1,23 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 //Al realizar el click mostrar y ocultar el menu
 menuEmail.addEventListener('click', toggleDesktopMenu);
+//Al realizar el click mostrar y ocultar el menu mobile
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-//Funcion Para poner o quitar el desktopMenu, classlist permite trabajarcon el objeto y .toggle lo activa o inactiva
+//Funcion Para poner o quitar el desktopMenu, classlist permite trabajar con el objeto y .toggle lo activa o inactiva
 function toggleDesktopMenu()
 {
   console.log('click');
   desktopMenu.classList.toggle('inactive');
 }
 
-console.log("JS Funcionando");
+//Funcion Para poner o quitar el Menu Mobile, classlist permite trabajar con el objeto y .toggle lo activa o inactiva
+function toggleMobileMenu()
+{
+  mobileMenu.classList.toggle('inactive');
+}
